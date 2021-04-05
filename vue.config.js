83476ee3e22ed17e05cd511ef7847b5005daf31f
victorlmneves <info@vitorneves.com>
2021-04-05 17:41:39 +0100

    Updated deploy.yml

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
-  publicPath: process.env.GITHUB_ACTIONS === true
-    ? '/wine-beer-cellar-vuejs/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? process.env.BASE_URL
     : '/'
 }
